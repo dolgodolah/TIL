966d7ba365e553774618a31a4608fe7936b3d0ca
dolgodolah <dolgodolah@gmail.com>
2021-02-11 13:48:36 +0900

    정답 추가

---
+++ algorithm/Programmers/카카오 코딩테스트/2019 카카오 개발자 겨울 인턴십_징검다리 건너기.py
@@ -0,0 +1,32 @@
+# 징검다리를 건너야하는 니니즈 친구들의 수를 이분탐색한다.
+# 니니즈 친구들의 수를 mid라고 했을 때 stones[idx]-mid가 0보다 작을 때
+# 즉 여러칸을 건너뛰어야할 때가 k번 '연속'으로 발생할 때 친구들의 수를 줄여보아 최솟값을 찾는다.
+# k번 '연속'으로 발생하지 않을 때(건너는 친구들이 적다는 뜻)는 left=mid+1를 통해 친구들의 수를 추가해본다.
+
+
+def solution(stones, k):
+    answer = 0
+    
+    left=0
+    right=200000000
+    while left<=right:
+        mid = (left+right)//2
+        cnt=0
+        val=False
+        for stone in stones:
+            if stone-mid<=0:
+                cnt+=1
+            else:
+                cnt=0
+            if cnt==k:
+                val=True
+                break
+        if val:
+            right=mid-1
+            answer=mid
+        else:
+            left=mid+1
+
+    return answer
+
+print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1],3))
