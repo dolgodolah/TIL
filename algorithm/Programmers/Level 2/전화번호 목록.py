cbd50b6f50b26e0c06bbeec8d80acc61840e44a2
dolgodolah <dolgodolah@gmail.com>
2021-02-15 16:08:04 +0900

    정답 추가

---
+++ algorithm/Programmers/Level 2/전화번호 목록.py
@@ -0,0 +1,29 @@
+# 1차 풀이
+# for i in phone_book:
+#     for j in phone_book:
+#         if i!=j and i==j[:len(i)]:
+#             answer=False
+#             break
+#     if answer==False:
+#         break
+
+# 1차 풀이도 효율성테스트까지 다 통과되는데 2차 풀이가 훨씬 더 보기좋은 소스코드인것같다.
+# 1차 풀이의 경우 i='1234', j='123'일때 j[:len(i)]를 해도 인덱싱에러가 뜨지않는다..(j의 길이보다 len(i)가 크지만 j의 최대길이만큼 가르킨다)
+# 2차 풀이가 다른 언어에서도 가능한 풀이같다.
+# 문자열들로 이루어진 리스트를 정렬하면 알파벳(사전)순으로 정렬이 된다.
+# ['b','aa','a'] -> ['a','aa','b']
+# ['123','12','3'] -> ['12','123','3']
+# 즉 phone_book[i]와 phone_book[i+1]끼리 비교해주면 된다.
+
+# 2차 풀이
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] in phone_book[i+1]:
+            answer=False
+            break
+
+    return answer
+
+print(solution(['123','456','789']))
