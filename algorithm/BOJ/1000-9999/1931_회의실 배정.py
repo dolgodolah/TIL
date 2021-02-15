a3716e397e149c187738c9f9038044ed3d54ab16
dolgodolah <dolgodolah@gmail.com>
2021-02-15 16:37:02 +0900

    정답 추가

--- algorithm/BOJ/1000-9999/1931_회의실 배정.py
+++ algorithm/BOJ/1000-9999/1931_회의실 배정.py
@@ -16,7 +16,7 @@ for _ in range(n):
     start,end=map(int,input().split())
     meeting.append((start,end))
 
-meeting.sort(key=lambda x:x[1])
+meeting.sort(key=lambda x:(x[1],x[0]))
 
 time=0
 answer=0
