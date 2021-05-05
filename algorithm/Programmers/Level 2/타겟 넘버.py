6bc7e87776d72fd09bb92b626fa1d93b8a9ed158
dolgodolah <dolgodolah@gmail.com>
2021-05-05 19:02:25 +0900

    +2 그래프이론(bfs)

--- algorithm/Programmers/Level 2/타겟 넘버.py
+++ algorithm/Programmers/Level 2/타겟 넘버.py
@@ -1,3 +1,23 @@
+# 21년 5월 5일 두번째 풀이
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue=deque()
+    queue.append((0,0))
+    while queue:
+        x,cnt=queue.popleft()
+        # print(x,cnt)
+        if cnt==len(numbers) and x==target:
+            answer+=1
+        if cnt+1<=len(numbers):
+            queue.append((x+numbers[cnt],cnt+1))
+            queue.append((x-numbers[cnt],cnt+1))
+    return answer
+
+print(solution([1,1,1,1,1],3))
+
+
+# 21년 1월 25일 첫번째 풀이
 answer = 0
 def dfs(cnt,result,numbers,target):
     global answer
