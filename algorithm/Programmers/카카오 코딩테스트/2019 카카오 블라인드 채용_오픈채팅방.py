5f7c18ee4b65a3f0b05ecb3991b7eb02cad35b20
dolgodolah <dolgodolah@gmail.com>
2021-05-05 19:03:23 +0900

    +2 문자열 처리, 해시(자료구조)

--- algorithm/Programmers/카카오 코딩테스트/2019 카카오 블라인드 채용_오픈채팅방.py
+++ algorithm/Programmers/카카오 코딩테스트/2019 카카오 블라인드 채용_오픈채팅방.py
@@ -1,3 +1,31 @@
+# 21년 5월 5일 두번째 풀이
+def solution(record):
+    answer = []
+    logs = []
+    dic = dict()
+    for command in record:
+        command = command.split()
+        if command[0]=="Enter":
+            logs.append((command[1],command[0])) #id, command종류
+            if not command[1] in dic:
+                dic[command[1]]=command[2]
+            else:
+                dic[command[1]]=command[2]
+        elif command[0]=="Leave":
+            logs.append((command[1],command[0]))
+        else: # Change
+            dic[command[1]]=command[2]
+
+
+    for log in logs:
+        if log[1]=="Enter":
+            answer.append(str(dic[log[0]])+"님이 들어왔습니다.")
+        else:
+            answer.append(str(dic[log[0]]+"님이 나갔습니다."))
+    return answer
+
+
+# 21년 1월 28일 첫번째 풀이
 #userId에 대한 정보를 딕셔너리에 저장해 닉네임이 바뀌면 해당 id의를 찾아 닉네임을 바꿔주도록 한다.
 #record가 들어오면 Enter,Leave,Change에 대한 정보와 userId의 정보만 따서 log에 담는다.
 #change의 경우 dic[userId]의 값을 해당 닉네임으로 바꾸는 처리를 해준다.
