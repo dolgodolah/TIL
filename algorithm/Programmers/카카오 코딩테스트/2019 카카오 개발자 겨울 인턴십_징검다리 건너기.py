ee1719570f6fe392154d8e7ee4731dc10c6fc411
dolgodolah <dolgodolah@gmail.com>
2021-05-08 18:01:17 +0900

    +2 이분탐색♥

--- algorithm/Programmers/카카오 코딩테스트/2019 카카오 개발자 겨울 인턴십_징검다리 건너기.py
+++ algorithm/Programmers/카카오 코딩테스트/2019 카카오 개발자 겨울 인턴십_징검다리 건너기.py
@@ -1,3 +1,26 @@
+# 21년 5월 7일
+def solution(stones, k):
+    answer = 0
+    left=1
+    right=max(stones)
+    while left<=right:
+        mid=(left+right)//2
+        cnt=0
+        for stone in stones:
+            if stone-mid<=0:
+                cnt+=1
+            else:
+                cnt=0
+            if cnt==k:
+                right=mid-1
+                answer=mid
+                break
+        else:
+            left=mid+1
+    return answer
+
+
+# 21년 2월 11일
 # 징검다리를 건너야하는 니니즈 친구들의 수를 이분탐색한다.
 # 니니즈 친구들의 수를 mid라고 했을 때 stones[idx]-mid가 0보다 작을 때
 # 즉 여러칸을 건너뛰어야할 때가 k번 '연속'으로 발생할 때 친구들의 수를 줄여보아 최솟값을 찾는다.
