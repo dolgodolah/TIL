215b5eeb2da11b3d9d5cfd984802217a9b528d92
dolgodolah <dolgodolah@gmail.com>
2021-05-10 19:08:14 +0900

    +2 완전탐색(조합)

--- algorithm/Programmers/카카오 코딩테스트/2019 카카오 블라인드 채용_후보키.py
+++ algorithm/Programmers/카카오 코딩테스트/2019 카카오 블라인드 채용_후보키.py
@@ -1,3 +1,53 @@
+#21년 5월 6일
+# 유일성과 최소성을 확인하는 로직을 check_uniqueness와 check_minimality로 나눴다.
+# combinations를 통해 모든 column의 조합들을 탐색한다.
+
+# 유일성을 만족하는지 확인하는 check_uniqueness는
+# 해당 columns로 튜플을 뽑아보면서 중복 발생 시 False를 반환하고, 중복이 발생하지 않는다면 True를 반환하여 유일성 만족여부를 알린다.
+
+# 최소성을 만족하는지 확인하는 check_minimality는
+# 해당 columns의 조합들중 이미 구해진 candidate_key에 속하는지 확인한다.
+# 포함이 된다면 최소성이 만족하지 않으므로 False를 반환한다. (columns=(0,1,2), candidate_key=[(0,1), ..])
+
+# 길이가 짧은 combinations부터 구하기때문에 차례대로 탐색하면 유일성과 최소성 판단할 수 있다.
+from itertools import combinations
+
+def check_uniqueness(columns,relation):
+    result = []
+    for row in range(len(relation)):
+        tmp=[]
+        for column in columns:
+            tmp.append(relation[row][column])
+        if not tmp in result:
+            result.append(tmp)
+        else:
+            return False
+    return True
+
+def check_minimality(columns,candidate_key):
+    # print(columns,candidate_key)
+    for i in range(1,len(columns)): #len(columns)+1를 하지 않아도 되는 이유는 이미 유일성을 만족한 columns이기 때문에 검증됐다.
+        for j in combinations(columns,i):
+            if j in candidate_key:
+                return False
+
+    return True
+
+
+def solution(relation):
+    answer = 0
+    candidate_key = []
+    for i in range(1,len(relation[0])+1):
+        for columns in combinations(range(len(relation[0])),i):
+            if check_uniqueness(columns,relation) and check_minimality(columns,candidate_key):
+                answer+=1
+                candidate_key.append(columns)
+    # print(candidate_key)
+    return answer
+
+
+
+#21년 1월 29일
 #배운거밖에 없었던 문제였다. 
 
 # relation에 있는 값을 한번에 여러 개 가져와야했다.
