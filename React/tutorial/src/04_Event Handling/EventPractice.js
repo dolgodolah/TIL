5107921da74ab3debc83f6aff38731cd1bca6789
garfield.kim(김규범)/kakao <dolgodolah@gmail.com>
2021-12-02 21:10:01 +0900

    useState에 객체 넣기

--- React/tutorial/src/04_Event Handling/EventPractice.js
+++ React/tutorial/src/04_Event Handling/EventPractice.js
@@ -1,19 +1,31 @@
 import {useState} from "react";
 
 const EventPractice = () => {
-    const [message, setMessage] = useState('')
-    const [username, setUsername] = useState('')
-    const onChangeUsername = (e) => setUsername(e.target.value)
-    const onChangeMessage = (e) => setMessage(e.target.value)
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const { username, message } = form;
+    const onChange = (e) => {
+        const nextForm = {
+            ...form, // 기존의 form 내용을 이 자리에 복사하고,
+            [e.target.name]: e.target.value // 원하는 값을 덮어 씌워야 함
+        };
+        setForm(nextForm);
+    };
+
     const onKeyPress = (e) => {
         if (e.key === 'Enter') {
             onClick();
         }
     }
+
     const onClick = () => {
         alert(username + ' : ' + message)
-        setMessage('');
-        setUsername('');
+        setForm({
+            username: '',
+            message: ''
+        })
     }
     return (
         <div>
@@ -23,13 +35,13 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명"
                 value={username}
-                onChange={ onChangeUsername }/>
+                onChange={ onChange }/>
             <input
                 type="text"
                 name="message"
                 placeholder="아무거나 입력해 보세요"
                 value={message}
-                onChange={ onChangeMessage }
+                onChange={ onChange }
                 onKeyPress={ onKeyPress }
             />
             <button onClick={ onClick }>확인</button>
