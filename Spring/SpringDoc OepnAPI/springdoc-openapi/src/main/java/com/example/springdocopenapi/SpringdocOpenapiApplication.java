902ac8c359de1bd8e7bab33fe7c2e6f52fadf709
garfield-kim <dolgodolah@gmail.com>
2023-11-24 19:46:01 +0900

    SpringDoc OpenAPI로 REST API 문서화 초기 세팅

---
+++ Spring/SpringDoc OepnAPI/springdoc-openapi/src/main/java/com/example/springdocopenapi/SpringdocOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocOpenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdocOpenapiApplication.class, args);
+	}
+
+}
